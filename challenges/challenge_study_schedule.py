134f01b358debcfaea1bcde89be1851d05b76fcd
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-25 16:29:10 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    contador = 0
+
+    if target_time == 0 or target_time is None:
+        return None
+
+    for tempo in permanence_period:
+        if tempo[0] <= target_time <= tempo[1]:
+            contador + 1
+    return contador
