70ce277040ebe5d93e94ac2aa035a021cb885271
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-26 13:26:32 -0300

    arrumando req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    contador = 0
+    try:
+        contador = 0
 
-    if target_time == 0 or target_time is None:
+        if target_time == 0 or target_time is None:
+            return None
+        for tempo in permanence_period:
+            if not isinstance(tempo[0], int) or not isinstance(tempo[1], int):
+                return None
+            if tempo[0] <= target_time <= tempo[1]:
+                contador += 1
+        return contador
+    except TypeError:
         return None
-
-    for tempo in permanence_period:
-        if tempo[0] <= target_time <= tempo[1]:
-            contador + 1
-    return contador
