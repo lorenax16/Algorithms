7650711ade1c229ef11114382d041edb6b5e2667
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-26 15:19:30 -0300

    req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+    with raises(TypeError):
+        encrypt_message(1, 1)
+        encrypt_message("lorena", 1)
+
+    assert encrypt_message("lorena", -1) == "anerol"
+    assert encrypt_message("lorena", 3) == "rol_ane"
+    assert encrypt_message("lorena", 4) == "an_erol"
+    assert encrypt_message("lorena", 5) == "nerol_a"
