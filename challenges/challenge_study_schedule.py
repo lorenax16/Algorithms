89404e2c451a1830aaa10d50ef930169af0add8e
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-26 14:16:31 -0300

    req 1 arrumando complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,6 @@
 def study_schedule(permanence_period, target_time):
     try:
         contador = 0
-
-        if target_time == 0 or target_time is None:
-            return None
         for tempo in permanence_period:
             if not isinstance(tempo[0], int) or not isinstance(tempo[1], int):
                 return None
