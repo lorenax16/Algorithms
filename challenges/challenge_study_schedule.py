5874d9e89dda19b50df3f8f200dc1283411b0cf0
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-26 14:44:30 -0300

    tirando o try para tirar complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    try:
-        contador = 0
-        for tempo in permanence_period:
-            if not isinstance(tempo[0], int) or not isinstance(tempo[1], int):
-                return None
-            if tempo[0] <= target_time <= tempo[1]:
-                contador += 1
-        return contador
-    except TypeError:
+    if target_time is None:
         return None
+    contador = 0
+    for tempo in permanence_period:
+        if not isinstance(tempo[0], int) or not isinstance(tempo[1], int):
+            return None
+        if tempo[0] <= target_time <= tempo[1]:
+            contador += 1
+    return contador
