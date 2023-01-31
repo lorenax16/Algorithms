11f9eae55516738f56db2f2fe6672bdecd38aad2
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-31 13:36:29 -0300

    req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,34 +1,47 @@
-def insertion_sort(string):
-    s = len(string)
-
-    for index in range(0, s):
-        chave = string[index]  # pego o segundo elemento da lista
-
-        new_position = index - 1  # tomamos a posição anterior para comparar
-
-        while (
-            new_position >= 0
-            and string[new_position] > string[new_position + 1]
-        ):
-            string[new_position + 1] = string[new_position]
-            new_position = new_position - 1
-
-        string[new_position + 1] = chave
-
-        return string
-
-
 def is_anagram(first_string, second_string):
     first = list(first_string.lower())
     second = list(second_string.lower())
 
-    insertion_sort(first)
-    insertion_sort(second)
+    if first_string == "" and second_string == "":
+        return (first_string, second_string, False)
 
-    if first == "" or second == "":
-        return False
+    merge_sort(first)
+    merge_sort(second)
 
-    if first == second:
-        return ("".join(first), "".join(second), True)
+    palavra1 = "".join(first)
+    palavra2 = "".join(second)
+
+    if palavra1 == palavra2:
+        return (palavra1, palavra2, True)
     else:
-        return ("".join(first), "".join(second), False)
+        return (palavra1, palavra2, False)
+
+
+def merge_sort(string, start=0, end=None):
+    if end is None:
+        end = len(string)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(string, start, mid)
+        merge_sort(string, mid, end)
+        merge(string, start, mid, end)
+
+
+def merge(string, start, mid, end):
+    left = string[start:mid]
+    right = string[mid:end]
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
